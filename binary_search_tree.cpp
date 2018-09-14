9f0f73fa7c58ccd43fa37c744a4ccd6a684daaa8
HanXu <601042507@qq.com>
2018-09-14 21:22:47 +0800

    二叉搜索树的更新

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -4,93 +4,82 @@
 	> Mail: 
 	> Created Time: 二  9/ 4 20:48:48 2018
  ************************************************************************/
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <time.h>
 typedef struct BSTNode{
     int key;
     struct BSTNode *lchild, *rchild;
 }BSTNode;
 
-BSTNode *get_new_node(int key){
+BSTNode *init(int key){
     BSTNode *p = (BSTNode *)malloc(sizeof(BSTNode));
-    p->lchild = p->rchild = NULL;
     p->key = key;
+    p->lchild = p->rchild = NULL;
     return p;
 }
 
-BSTNode *insert(BSTNode *tree, int key){
-    if(tree == NULL) tree = get_new_node(key);
-    if(tree->key == key) return tree;
-    if(key < tree->key) tree->lchild = insert(tree->lchild, key);
-    else tree->rchild = insert(tree->rchild, key);
-    return tree;
-}
-
-BSTNode *predecessor(BSTNode *tree){
-    BSTNode *p = tree->lchild;
-    while(p->rchild){
-        p = p->rchild;
-    }
-    return p;
-}
-
-BSTNode *delete_node(BSTNode *tree, int key){
-    if(tree == NULL) return NULL;
-    if(key < tree->key){
-        tree->lchild =  delete_node(tree->lchild, key);
-    }else if(key > tree->key){
-        tree->rchild =  delete_node(tree->rchild, key);
+BSTNode *insert(BSTNode *root, int key){
+    if(root == NULL) return init(key);
+    if(root->key == key) return root;
+    if(root->key > key){
+        root->lchild = insert(root->lchild, key);
     }else{
-        if(tree->lchild == NULL && tree->rchild == NULL){
-            free(tree);
-            return NULL;
-        }else if(tree->lchild == NULL || tree->rchild == NULL){
-            BSTNode *ret_tree = (tree->lchild ? tree->lchild : tree->rchild);
-            free(tree);
-            return ret_tree;
-        }else{
-            BSTNode *pre_node = predecessor(tree);
-            tree->key = pre_node->key;
-            tree->lchild = delete_node(tree->lchild, pre_node->key);
-        }
+        root->rchild = insert(root->rchild, key);
     }
-    return tree;
+    return root;
 }
 
-int search(BSTNode *tree, int key){
-    if(tree == NULL) return 0;
-    if(tree->key == key) return 1;
-    if(key < tree->key) return search(tree->lchild, key);
-    return search(tree->rchild, key);
+BSTNode *predecessor(BSTNode *node){
+    BSTNode *temp = node->lchild;
+    while(temp->rchild) temp = temp->rchild;
+    return temp;
 }
 
-void in_order(BSTNode *tree){
-    if(tree == NULL) return ;
-    in_order(tree->lchild);
-    printf("%d ", tree->key);
-    in_order(tree->rchild);
-    return ;
-}
-
-void clear(BSTNode *tree){
-    if(tree == NULL) return ;
-    clear(tree->lchild);
-    clear(tree->rchild);
-    free(tree);
-    return ;
-}
-
-int main(){
-    int op, value;
-    BSTNode *tree = NULL;
-    while(~scanf("%d%d", &op, &value)){
-        switch(op){
-            case 1: tree = insert(tree, value);break;
-            case 2: tree = delete_node(tree, value); break;
-            case 3: printf("search(%d) = %d\n", value, search(tree, value));break;
-            case 4: in_order(tree);printf("\n");break;
+BSTNode *delete_node(BSTNode *root, int key){
+    if(root == NULL) return root;
+    if(root->key == key){
+        if(root->lchild == NULL && root->rchild == NULL){
+            free(root);
+            return NULL;
+        }else if(root->lchild == NULL || root->rchild == NULL){
+            BSTNode *temp = root->lchild ? root->lchild : root->rchild;
+            free(root);
+            return temp;
+        }else{
+            BSTNode *temp = predecessor(root);
+            root->key = temp->key;
+            root->lchild = delete_node(root->lchild, temp->key);
         }
+    }else{
+        if(root->key < key) root->rchild = delete_node(root->rchild, key);
+        else root->lchild = delete_node(root->lchild, key);
+    }
+    return root;
+}
+
+void output(BSTNode *root){
+    if(root == NULL) return;
+    output(root->lchild);
+    printf("%d ", root->key);
+    output(root->rchild);
+    return ;
+}
+int main(){
+    srand(time(0));
+    BSTNode *root = NULL;
+    #define OP_NUM 20
+    for(int i = 0; i < OP_NUM; i++){
+        int key = rand() % 30;
+        root = insert(root, key);
+        printf("insert : %d to tree\n", key);
+        output(root), printf("\n");
+    }
+    for(int i = 0; i < OP_NUM; i++){
+        int key = rand() % 30;
+        root = delete_node(root, key);
+        printf("delete_node: %d to tree\n", key);
+        output(root), printf("\n");
     }
     return 0;
 }
