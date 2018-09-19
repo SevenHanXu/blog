8c6ba6b216f6b2bc8233fdf2c32b49f58d9ed388
HanXu <601042507@qq.com>
2018-09-19 16:31:49 +0800

    一般读入

---
+++ low_read.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: low_read.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 三  9/19 16:14:17 2018
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n, a;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a);
+    }
+    return 0;
+}
