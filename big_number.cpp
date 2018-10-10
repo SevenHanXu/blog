5591a5a0de0e056bb84f69a4871ff01ff5cd1375
HanXu <601042507@qq.com>
2018-10-10 15:16:07 +0800

    优化版---在数组中求[a, b]范围内的最大值

--- big_number.cpp
+++ big_number.cpp
@@ -12,32 +12,29 @@
 
 int arr[MAX_N + 5][MAX_K + 5] = {0};
 
+int find(int a, int b, int k){
+   int ind = a, num = arr[a][0]; 
+    for(int i = k; i >= 0; i--){
+        if(ind == b) break;
+        if(ind + (1 << i) - 1 > b) continue;
+        do{
+            num = num > arr[ind][i] ? num : arr[ind][i];
+            ind += (1 << i);
+        }while(ind + (1 << i) - 1 <= b);
+    }
+    return num;
+}
+
 void init(int n){
     for(int k = 1; k <= MAX_K; k++){
-        for(int i = 0; i < n; i++){
-            int num = 0, ind = i;
-            for(int j = k - 1; j >= 0; j--){
-                if(ind + (1 << j ) > n) continue;
-                do{
-                    num = num > arr[ind][j] ? num : arr[ind][j];
-                    ind += (1 << j);
-                }while(ind + (1 << j) <= n);
-            }
-            arr[i][k] = num; 
+        for(int i = 0; i < n && i + (1 << k) <= n; i++){
+            arr[i][k] = find(i, i+ (1 << k) - 1, k - 1); 
         }
     }
     return ;
 }
 
-int find(int a, int b){
-   int ind = a, num = 0, len = b - a + 1;
-    for(int i = MAX_K; i >= 0; i--){
-        if((len & (1 << i)) == 0) continue;
-        num = num > arr[ind][i] ? num : arr[ind][i];
-        ind += (1 << i);
-    }
-    return num;
-}
+
 int main(){
     int n, a, b;
     scanf("%d", &n);
@@ -46,7 +43,7 @@ int main(){
     }
     init(n);
     while(scanf("%d%d", &a, &b) != EOF){
-        printf("%d\n", find(a, b));
+        printf("%d\n", find(a, b, MAX_K));
     }
     return 0;
 }
